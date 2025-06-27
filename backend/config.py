d2b379a1c5d86344f960fe4864e6c40a1dfec925
Aaron Casas <aaroncasas27@outlook.com>
2025-06-26 22:45:42 -0600

    Update database url

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,3 @@
-# config.py
 import os
 from dotenv import load_dotenv
 
@@ -6,7 +5,9 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  
+    SQLALCHEMY_TRACK_MODIFICATIONS = False               
     PREFERRED_URL_SCHEME = "https"
     SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SECURE = False
+    SESSION_COOKIE_SECURE = False  # Set to True if you're enforcing HTTPS
     REMEMBER_COOKIE_HTTPONLY = True
