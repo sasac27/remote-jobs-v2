2bab9019598e8917982543a27f731edf86cc2ead
Aaron Casas <aaroncasas27@outlook.com>
2025-06-26 21:52:22 -0600

    Update paths for render

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,8 +1,8 @@
-from routes.api_auth import api_auth_bp
-from routes.subscriptions import sub_bp
-from routes.jobs import jobs_bp 
-from routes.analytics import analytics_bp
-from routes.dashboard import dashboard_bp
+from backend.routes.api_auth import api_auth_bp
+from backend.routes.subscriptions import sub_bp
+from backend.routes.jobs import jobs_bp 
+from backend.routes.analytics import analytics_bp
+from backend.routes.dashboard import dashboard_bp
 
 def register_routes(app):
     app.register_blueprint(api_auth_bp)
