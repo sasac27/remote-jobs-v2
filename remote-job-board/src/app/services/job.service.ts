50424b4b2601d441964d4137d626be9708fc50d8
Aaron Casas <aaroncasas27@outlook.com>
2025-06-27 22:00:11 -0600

    Update apiURL for JobService

--- remote-job-board/src/app/services/job.service.ts
+++ remote-job-board/src/app/services/job.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environment';
 
 export interface Job {
   url: string;
@@ -18,14 +19,16 @@ export interface Job {
   providedIn: 'root'
 })
 export class JobService {
-  private apiUrl = 'http://localhost:5000/api/jobs';
+  private apiUrl = `${environment.apiUrl}/jobs`;
 
   constructor(private http: HttpClient) {}
 
   getJobs(paramsObj: any): Observable<Job[]> {
     let params = new HttpParams();
     for (let key in paramsObj) {
-      if (paramsObj[key]) params = params.set(key, paramsObj[key]);
+      if (paramsObj[key]) {
+        params = params.set(key, paramsObj[key]);
+      }
     }
     return this.http.get<Job[]>(this.apiUrl, { params });
   }
