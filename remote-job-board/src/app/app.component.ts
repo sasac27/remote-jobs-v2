0d6f48764013e88acc52d41b3397483c40e19ecb
Aaron Casas <aaroncasas27@outlook.com>
2025-07-08 14:38:46 -0600

    Delete Footer, unused

--- remote-job-board/src/app/app.component.ts
+++ remote-job-board/src/app/app.component.ts
@@ -2,12 +2,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent],
   template: `
     <app-navbar></app-navbar>
 
