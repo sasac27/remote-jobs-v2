7d8e3e00d4d9c24000ab04594035e1ac69858717
Aaron Casas <aaroncasas27@outlook.com>
2025-07-11 14:00:30 -0600

    Update register to use render url

--- remote-job-board/src/app/interceptors/auth.interceptor.ts
+++ remote-job-board/src/app/interceptors/auth.interceptor.ts
@@ -1,20 +1,25 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { AuthService } from '../services/auth.service';
 
 export const authInterceptorFn: HttpInterceptorFn = (req, next) => {
-  const auth = inject(AuthService);
-  const token = auth.getToken();
+  const platformId = inject(PLATFORM_ID);
 
-  if (token) {
-    console.log('✅ Attaching token:', token);
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
-      },
-    });
-  } else {
-    console.warn('❌ No token found in localStorage');
+  if (isPlatformBrowser(platformId)) {
+    const auth = inject(AuthService);
+    const token = auth.getToken();
+
+    if (token) {
+      console.log('✅ Attaching token:', token);
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } else {
+      console.warn('❌ No token found in localStorage');
+    }
   }
 
   return next(req);
--- remote-job-board/src/app/services/subscription.service.ts
+++ remote-job-board/src/app/services/subscription.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubscriptionService {
-  private API_URL = 'http://localhost:5000/api/subscriptions';
+  private API_URL = `${environment.apiUrl}/subscriptions`;
 
   constructor(private http: HttpClient) {}
 
